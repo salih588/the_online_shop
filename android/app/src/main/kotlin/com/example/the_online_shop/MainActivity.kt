246b4427abd8e1dfa643cd975efc2789e3450fa8
salihnsventures <muhammed.salih@nsventures.se>
2024-03-20 14:20:52 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/the_online_shop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.the_online_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'The Online Shop',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(children: [
+          Text('The Online Shop'),
+        ],),
+      ) // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
